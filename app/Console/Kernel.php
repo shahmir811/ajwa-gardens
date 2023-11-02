c7a05941e8ea1a63fc4aa209034e779334887bc5
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2023-11-02 14:53:34 +0500

    Added sending SMS work during booking and instalment

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
 
     protected $commands = [
         Commands\BackupCron::class,
+        Commands\SendSMS::class,
     ];
 
     protected function schedule(Schedule $schedule): void
